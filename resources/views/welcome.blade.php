f369fe6891a5cc7d26787fb9efbf2dbea9a185ef
pradeepprajapati9 <kunalpatel9953@gmail.com>
2024-04-10 11:00:19 +0530

    welcome files fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,12 @@
     </style>
 </head>
 <body>
+    <h1>found a assignment from siec education pvt ltd in gurgoan hariyana</h1>
     <h2>system using linux ubantu </h2>
     
     <h1>hi my name is pradeep fetching the api fake json display with blade template</h1>
 
-    <p>for showing the go to url bar write <b>/post</b> then enter</p>
+    <p>for showing the data go to url bar write <b>/post</b> then enter</p>
     <p>create Apicontroller first</p>
     <p>make show.blade.php</p>
     <p>make index.blade.php </p>
